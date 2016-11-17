6575998bd61ce3a684e76af3675485bcd4a8a940
kesong <800578@gmail.com>
2016-11-17 12:16:55 +0100

    [R] bugfixing for bleChallengerService with AES cryption

--- SecureAccessBLE/Classes/BLEChallenger/BLEChallengeService.swift
+++ SecureAccessBLE/Classes/BLEChallenger/BLEChallengeService.swift
@@ -140,8 +140,8 @@ struct BLEChallengeService {
         self.leaseTokenId = leaseTokenId
         self.sidAccessKey = sidAccessKey
         
-        self.nc = [0x0F,0x0E,0x0D,0x0C,0x0B,0x0A,0x09,0x08,0x07,0x06,0x05,0x04,0x03,0x02,0x01,0x01] as [UInt8]
-        //self.nc = Cipher.randomIV(16)
+//        self.nc = [0x0F,0x0E,0x0D,0x0C,0x0B,0x0A,0x09,0x08,0x07,0x06,0x05,0x04,0x03,0x02,0x01,0x01] as [UInt8]
+        self.nc = AES.randomIV(16)
         
         guard let sharedKey = self.sidAccessKey.dataFromHexadecimalString() else {
             return nil
@@ -150,7 +150,7 @@ struct BLEChallengeService {
         let key = sharedKey.bytes//.arrayOfBytes()
         
         do {
-            let aesCrypto = try AES(key: key)
+            let aesCrypto = try AES(key: key, padding: ZeroByte())
             self.crypto = aesCrypto
         } catch {
             return nil
