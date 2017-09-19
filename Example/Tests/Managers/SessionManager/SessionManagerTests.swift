8ab748da964d996ee3a1f7921a087dcaeab7eddb
Torsten Lehmann <dev@tlehmann.com>
2017-09-19 13:44:28 +0200

    PLAM-959: Add SessionManager tests

--- Example/Tests/Managers/SessionManager/SessionManagerTests.swift
+++ Example/Tests/Managers/SessionManager/SessionManagerTests.swift
@@ -13,6 +13,14 @@ private let sorcIDA = UUID(uuidString: "be2fecaf-734b-4252-8312-59d477200a20")!
 private let sorcAccessKey = "1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a"
 private let leaseTokenA = try! LeaseToken(id: "", leaseID: "", sorcID: sorcIDA, sorcAccessKey: sorcAccessKey)
 private let leaseTokenBlobA = try! LeaseTokenBlob(messageCounter: 1, data: "1a")
+private let serviceGrantIDA = ServiceGrantID(2)
+private let serviceGrantIDB = ServiceGrantID(3)
+private let serviceGrantIDAResponse = ServiceGrantID(4)
+private let serviceGrantAResponseMessage = SorcMessage(rawData: Data(bytes: [
+    SorcMessageID.serviceGrantTrigger.rawValue,
+    UInt8(serviceGrantIDAResponse), 0x00,
+    ServiceGrantResponse.Status.pending.rawValue,
+]))
 
 private class MockSecurityManager: SecurityManagerType {
 
@@ -47,6 +55,8 @@ class SessionManagerTests: XCTestCase {
 
         sessionManager = SessionManager(securityManager: securityManager)
     }
+
+    // MARK: - Tests
 
     func test_init_succeeds() {
         XCTAssertNotNil(sessionManager)
@@ -138,17 +148,8 @@ class SessionManagerTests: XCTestCase {
         // Given
         prepareTransportConnecting()
 
-        var receivedConnectionChange: ConnectionChange!
-        _ = sessionManager.connectionChange.subscribeNext { change in
-            receivedConnectionChange = change
-        }
-
-        // When
-        sessionManager.connectToSorc(leaseToken: leaseTokenA, leaseTokenBlob: leaseTokenBlobA)
-
-        // Then
-        XCTAssertNil(securityManager.connectToSorcCalledWithArguments)
-        XCTAssertEqual(receivedConnectionChange.action, .initial)
+        // When // Then
+        connectToSorcAndAssertDoesNothing()
     }
 
     func test_connectToSorc_ifChallengingConnecting_doesNothing() {
@@ -156,17 +157,8 @@ class SessionManagerTests: XCTestCase {
         // Given
         prepareChallengingConnecting()
 
-        var receivedConnectionChange: ConnectionChange!
-        _ = sessionManager.connectionChange.subscribeNext { change in
-            receivedConnectionChange = change
-        }
-
-        // When
-        sessionManager.connectToSorc(leaseToken: leaseTokenA, leaseTokenBlob: leaseTokenBlobA)
-
-        // Then
-        XCTAssertNil(securityManager.connectToSorcCalledWithArguments)
-        XCTAssertEqual(receivedConnectionChange.action, .initial)
+        // When // Then
+        connectToSorcAndAssertDoesNothing()
     }
 
     func test_connectToSorc_ifConnected_doesNothing() {
@@ -174,17 +166,8 @@ class SessionManagerTests: XCTestCase {
         // Given
         prepareConnected()
 
-        var receivedConnectionChange: ConnectionChange!
-        _ = sessionManager.connectionChange.subscribeNext { change in
-            receivedConnectionChange = change
-        }
-
-        // When
-        sessionManager.connectToSorc(leaseToken: leaseTokenA, leaseTokenBlob: leaseTokenBlobA)
-
-        // Then
-        XCTAssertNil(securityManager.connectToSorcCalledWithArguments)
-        XCTAssertEqual(receivedConnectionChange.action, .initial)
+        // When // Then
+        connectToSorcAndAssertDoesNothing()
     }
 
     func test_disconnect_ifConnecting_disconnectsSecurityManagerAndNotifiesDisconnect() {
@@ -192,21 +175,8 @@ class SessionManagerTests: XCTestCase {
         // Given
         preparePhysicalConnecting()
 
-        var receivedConnectionChange: ConnectionChange!
-        _ = sessionManager.connectionChange.subscribeNext { change in
-            receivedConnectionChange = change
-        }
-
-        // When
-        sessionManager.disconnect()
-
-        securityManager.connectionChange.onNext(.init(state: .disconnected, action: .disconnect))
-
-        // Then
-        XCTAssertTrue(securityManager.disconnectCalled)
-
-        let expectedChange = ConnectionChange(state: .disconnected, action: .disconnect)
-        XCTAssertEqual(receivedConnectionChange, expectedChange)
+        // When // Then
+        disconnectAndAssertNotifiesDisconnect()
     }
 
     func test_disconnect_ifConnected_disconnectsAndNotifiesDisconnect() {
@@ -214,21 +184,8 @@ class SessionManagerTests: XCTestCase {
         // Given
         prepareConnected()
 
-        var receivedConnectionChange: ConnectionChange!
-        _ = sessionManager.connectionChange.subscribeNext { change in
-            receivedConnectionChange = change
-        }
-
-        // When
-        sessionManager.disconnect()
-
-        securityManager.connectionChange.onNext(.init(state: .disconnected, action: .disconnect))
-
-        // Then
-        XCTAssertTrue(securityManager.disconnectCalled)
-
-        let expectedChange = ConnectionChange(state: .disconnected, action: .disconnect)
-        XCTAssertEqual(receivedConnectionChange, expectedChange)
+        // When // Then
+        disconnectAndAssertNotifiesDisconnect()
     }
 
     func test_disconnect_ifNotConnectingOrConnected_doesNothing() {
@@ -246,6 +203,160 @@ class SessionManagerTests: XCTestCase {
         XCTAssertFalse(securityManager.disconnectCalled)
         XCTAssertEqual(receivedConnectionChange.action, .initial)
     }
+
+    func test_requestServiceGrant_ifConnectedAndQueueIsNotFullAndNotWaitingForResponse_itSendsMessageToSecurityManager() {
+
+        // Given
+        prepareConnected()
+
+        var receivedServiceGrantChange: ServiceGrantChange!
+        _ = sessionManager.serviceGrantChange.subscribeNext { change in
+            receivedServiceGrantChange = change
+        }
+
+        // When
+        sessionManager.requestServiceGrant(serviceGrantIDA)
+
+        // Then
+        let expectedMessage = SorcMessage(
+            id: SorcMessageID.serviceGrant,
+            payload: ServiceGrantRequest(serviceGrantID: serviceGrantIDA)
+        )
+        XCTAssertEqual(securityManager.sendMessageCalledWithMessage!, expectedMessage)
+
+        let expectedServiceGrantChange = ServiceGrantChange(
+            state: .init(requestingServiceGrantIDs: [serviceGrantIDA]),
+            action: .requestServiceGrant(id: serviceGrantIDA, accepted: true)
+        )
+        XCTAssertEqual(receivedServiceGrantChange, expectedServiceGrantChange)
+    }
+
+    func test_requestServiceGrant_ifConnectedAndQueueIsNotFullAndWaitingForResponse_itEnqueuesMessage() {
+
+        // Given
+        prepareConnected()
+
+        sessionManager.requestServiceGrant(serviceGrantIDA)
+
+        var receivedServiceGrantChange: ServiceGrantChange!
+        _ = sessionManager.serviceGrantChange.subscribeNext { change in
+            receivedServiceGrantChange = change
+        }
+
+        // When
+        sessionManager.requestServiceGrant(serviceGrantIDB)
+
+        // Then
+        let expectedMessage = SorcMessage(
+            id: SorcMessageID.serviceGrant,
+            payload: ServiceGrantRequest(serviceGrantID: serviceGrantIDA)
+        )
+        // did not receive serviceGrantIDB
+        XCTAssertEqual(securityManager.sendMessageCalledWithMessage!, expectedMessage)
+
+        let expectedServiceGrantChange = ServiceGrantChange(
+            state: .init(requestingServiceGrantIDs: [serviceGrantIDA, serviceGrantIDB]),
+            action: .requestServiceGrant(id: serviceGrantIDB, accepted: true)
+        )
+        XCTAssertEqual(receivedServiceGrantChange, expectedServiceGrantChange)
+    }
+
+    func test_requestServiceGrant_ifConnectedAndMessageIsEnqueuedAndReceivedServiceGrantResponse_itNotfifiesResponseAndItSendsEnqueuedMessage() {
+
+        // Given
+        prepareConnected()
+
+        sessionManager.requestServiceGrant(serviceGrantIDA)
+        sessionManager.requestServiceGrant(serviceGrantIDB)
+
+        var receivedServiceGrantChange: ServiceGrantChange!
+        _ = sessionManager.serviceGrantChange.subscribeNext { change in
+            receivedServiceGrantChange = change
+        }
+
+        // When
+        securityManager.messageReceived.onNext(.success(serviceGrantAResponseMessage))
+
+        // Then
+        let expectedServiceGrantChange = ServiceGrantChange(
+            state: .init(requestingServiceGrantIDs: [serviceGrantIDB]),
+            action: .responseReceived(.init(
+                sorcID: sorcIDA,
+                serviceGrantID: serviceGrantIDAResponse,
+                status: .pending,
+                responseData: ""
+            ))
+        )
+        XCTAssertEqual(receivedServiceGrantChange!, expectedServiceGrantChange)
+
+        let expectedMessage = SorcMessage(
+            id: SorcMessageID.serviceGrant,
+            payload: ServiceGrantRequest(serviceGrantID: serviceGrantIDB)
+        )
+        XCTAssertEqual(securityManager.sendMessageCalledWithMessage!, expectedMessage)
+    }
+
+    func test_requestServiceGrant_ifConnectedAndQueueIsFull_itDoesNotSendMessageAndItNotifiesNotAccepted() {
+
+        // Given
+        let configuration = SessionManager.Configuration(maximumEnqueuedMessages: 2)
+        sessionManager = SessionManager(securityManager: securityManager, configuration: configuration)
+        prepareConnected()
+
+        // added and instantly removed from queue
+        sessionManager.requestServiceGrant(serviceGrantIDB)
+        // enqueued and staying in queue
+        sessionManager.requestServiceGrant(serviceGrantIDB)
+        sessionManager.requestServiceGrant(serviceGrantIDB)
+        // not accepted, queue is full
+        sessionManager.requestServiceGrant(serviceGrantIDA)
+
+        var receivedServiceGrantChange: ServiceGrantChange!
+        _ = sessionManager.serviceGrantChange.subscribeNext { change in
+            receivedServiceGrantChange = change
+        }
+
+        // When
+        sessionManager.requestServiceGrant(serviceGrantIDA)
+
+        // Then
+        let unexpectedMessage = SorcMessage(
+            id: SorcMessageID.serviceGrant,
+            payload: ServiceGrantRequest(serviceGrantID: serviceGrantIDA)
+        )
+        XCTAssertNotEqual(securityManager.sendMessageCalledWithMessage!, unexpectedMessage)
+
+        let expectedServiceGrantChange = ServiceGrantChange(
+            state: .init(requestingServiceGrantIDs: [serviceGrantIDB, serviceGrantIDB, serviceGrantIDB]),
+            action: .requestServiceGrant(id: serviceGrantIDA, accepted: false)
+        )
+        XCTAssertEqual(receivedServiceGrantChange, expectedServiceGrantChange)
+    }
+
+    func test_requestServiceGrant_ifNotConnected_itDoesNothing() {
+
+        // Given
+        sessionManager.requestServiceGrant(serviceGrantIDA)
+
+        var receivedServiceGrantChange: ServiceGrantChange!
+        _ = sessionManager.serviceGrantChange.subscribeNext { change in
+            receivedServiceGrantChange = change
+        }
+
+        // When
+        sessionManager.requestServiceGrant(serviceGrantIDA)
+
+        // Then
+        XCTAssertNil(securityManager.sendMessageCalledWithMessage)
+
+        let expectedServiceGrantChange = ServiceGrantChange(
+            state: .init(requestingServiceGrantIDs: []),
+            action: .initial
+        )
+        XCTAssertEqual(receivedServiceGrantChange!, expectedServiceGrantChange)
+    }
+
+    // MARK: - State preparation
 
     private func preparePhysicalConnecting() {
         sessionManager.connectToSorc(leaseToken: leaseTokenA, leaseTokenBlob: leaseTokenBlobA)
@@ -273,6 +384,59 @@ class SessionManagerTests: XCTestCase {
         securityManager.connectionChange.onNext(.init(
             state: .connected(sorcID: sorcIDA),
             action: .connectionEstablished(sorcID: sorcIDA))
+        )
+    }
+
+    // MARK: - Helper
+
+    private func connectToSorcAndAssertDoesNothing(file: StaticString = #file, line: UInt = #line) {
+
+        var receivedConnectionChange: ConnectionChange!
+        _ = sessionManager.connectionChange.subscribeNext { change in
+            receivedConnectionChange = change
+        }
+
+        // When
+        sessionManager.connectToSorc(leaseToken: leaseTokenA, leaseTokenBlob: leaseTokenBlobA)
+
+        // Then
+        XCTAssertNil(
+            securityManager.connectToSorcCalledWithArguments,
+            "securityManager.connectToSorc() was called, but should not",
+            file: file,
+            line: line
+        )
+        XCTAssertEqual(
+            receivedConnectionChange.action,
+            .initial,
+            "connectionChange was received, but should not",
+            file: file,
+            line: line
+        )
+    }
+
+    private func disconnectAndAssertNotifiesDisconnect(file: StaticString = #file, line: UInt = #line) {
+
+        var receivedConnectionChange: ConnectionChange!
+        _ = sessionManager.connectionChange.subscribeNext { change in
+            receivedConnectionChange = change
+        }
+
+        // When
+        sessionManager.disconnect()
+
+        securityManager.connectionChange.onNext(.init(state: .disconnected, action: .disconnect))
+
+        // Then
+        XCTAssertTrue(securityManager.disconnectCalled, "securityManager.disconnect() was not called")
+
+        let expectedChange = ConnectionChange(state: .disconnected, action: .disconnect)
+        XCTAssertEqual(
+            receivedConnectionChange,
+            expectedChange,
+            "Should receive disconnected state and disconnect action, but did not",
+            file: file,
+            line: line
         )
     }
 }
