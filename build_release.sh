7119e878dad9bd7552513057bfa79e21d5f49d4e
Oleg Langer <oleg.langer@huf-group.com>
2019-09-04 14:54:41 +0200

    PLB2B-846: Add custom derived data path for carhage to avoid reusing artifacts

--- build_release.sh
+++ build_release.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 set -xe
 
-rm -rf BUILD
 cd Example
-rm -rf Carthage
+rm -rf ../BUILD Carthage DerivedData
+
 
 function run_carthage {
     # CARTHAGE_NO_VERBOSE flag turns off the verbose log of carthage which led to issues in jenkins job,
     # see https://github.com/Carthage/Carthage/issues/2249 for details.
     if [ ! -z "$CARTHAGE_NO_VERBOSE" ]; then
-        carthage build --no-skip-current
+        carthage build --no-skip-current --derived-data DerivedData
     else
-        carthage build --no-skip-current --verbose
+        carthage build --no-skip-current --verbose --derived-data DerivedData
     fi
 }
 
@@ -22,6 +22,7 @@ cp -R Carthage/Build/iOS/* ../BUILD/Dynamic
 cp -R ../distribution/* ../BUILD/Dynamic
 
 echo "Building static framework..."
+rm -rf Carthage DerivedData
 mkdir -p ../BUILD/Static
 
 echo "Changing MACH_O_TYPE of target to staticlib..." 
@@ -29,7 +30,7 @@ sed -i '' 's/MACH_O_TYPE.*/MACH_O_TYPE = staticlib;/g' SecureAccessBLE.xcodeproj
 echo "Reinstalling pods as static libs..."
 bundle exec pod deintegrate
 sed -i '' 's/use_frameworks!/ /g' Podfile
-bundle exec pod install --verbose
+pod install --verbose
 run_carthage
 
 cp -R Carthage/Build/iOS/Static/ ../BUILD/Static
