b2277fa1ccafefbf7c86e94203e010bed171fa91
Oleg Langer <oleg.langer@huf-group.com>
2019-09-06 13:21:16 +0200

    PLB2B-846: Update build script to accept static param

--- build_release.sh
+++ build_release.sh
@@ -4,34 +4,22 @@ set -xe
 cd Example
 rm -rf ../BUILD Carthage DerivedData
 
+if [ ! -z "$STATIC_BUILD" ]; then
+    OUTPUT_PATH=../BUILD/Dynamic
+    CARTHAGE_BUILD_PATH=Carthage/Build/iOS
+else
+    OUTPUT_PATH=../BUILD/Static
+    CARTHAGE_BUILD_PATH=Carthage/Build/iOS/Static
+fi
 
-function run_carthage {
-    # CARTHAGE_NO_VERBOSE flag turns off the verbose log of carthage which led to issues in jenkins job,
-    # see https://github.com/Carthage/Carthage/issues/2249 for details.
-    if [ ! -z "$CARTHAGE_NO_VERBOSE" ]; then
-        carthage build --no-skip-current --derived-data DerivedData
-    else
-        carthage build --no-skip-current --verbose --derived-data DerivedData
-    fi
-}
+# CARTHAGE_NO_VERBOSE flag turns off the verbose log of carthage which led to issues in jenkins job,
+# see https://github.com/Carthage/Carthage/issues/2249 for details.
+if [ ! -z "$CARTHAGE_NO_VERBOSE" ]; then
+    carthage build --no-skip-current --derived-data DerivedData --log-path carthage.log
+else
+    carthage build --no-skip-current --derived-data DerivedData --log-path carthage.log --verbose
+fi
 
-echo "Building dynamic framework..."
-run_carthage
-mkdir -p ../BUILD/Dynamic
-cp -R Carthage/Build/iOS/* ../BUILD/Dynamic
-cp -R ../distribution/* ../BUILD/Dynamic
-
-echo "Building static framework..."
-rm -rf Carthage DerivedData
-mkdir -p ../BUILD/Static
-
-echo "Changing MACH_O_TYPE of target to staticlib..." 
-sed -i '' 's/MACH_O_TYPE.*/MACH_O_TYPE = staticlib;/g' SecureAccessBLE.xcodeproj/project.pbxproj 
-echo "Reinstalling pods as static libs..."
-bundle exec pod deintegrate
-sed -i '' 's/use_frameworks!/ /g' Podfile
-pod install --verbose
-run_carthage
-
-cp -R Carthage/Build/iOS/Static/ ../BUILD/Static
-cp -R ../distribution/* ../BUILD/Static
+mkdir -p $OUTPUT_PATH
+cp -R ${CARTHAGE_BUILD_PATH}/* $OUTPUT_PATH
+cp -R ../distribution/* $OUTPUT_PATH
