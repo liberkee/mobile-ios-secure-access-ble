619cbe2312f30c4309aec74b3ca0aeeb662ab299
Oleg Langer <oleg.langer@huf-group.com>
2019-09-06 11:59:11 +0200

    PLB2B-846: Add script to prepare static config

---
+++ prepare_static.sh
@@ -0,0 +1,24 @@
+set -e
+
+CURRENT_TAG=$(git describe --exact-match --tags HEAD)
+if [ ! -z "$CURRENT_TAG" ]; then
+    echo "Current tag is ${CURRENT_TAG}"
+    BRANCH_NAME=release/${CURRENT_TAG}_static_framework
+    echo "Creating branch ${BRANCH_NAME}"
+    git checkout -b release/${CURRENT_TAG}_static_framework
+    cd Example
+    echo "Changing MACH_O_TYPE of target to staticlib..." 
+    sed -i '' 's/MACH_O_TYPE.*/MACH_O_TYPE = staticlib;/g' SecureAccessBLE.xcodeproj/project.pbxproj 
+    echo "Reinstalling pods as static libs..."
+    bundle exec pod deintegrate
+    sed -i '' 's/use_frameworks!/ /g' Podfile
+    bundle exec pod install --verbose
+    echo "Pod installiation finished."
+    echo "Commit..."
+    git add .
+    git commit -m "Prepare static release ${CURRENT_TAG}"
+    git push --set-upstream origin $(git_current_branch)
+else 
+    echo "No tag found. This script should only be executed on a commit which contains tag."
+fi
+
