32f7701ff22b985dd41ce83e293afb098ee6e992
Oleg Langer <oleg.langer@huf-group.com>
2020-03-30 15:32:37 +0200

    PLCII-960: Create payload from mobilebulk

--- Example/Tests/Model/BulkTransmitMessageTests.swift
+++ Example/Tests/Model/BulkTransmitMessageTests.swift
@@ -12,7 +12,7 @@ import Quick
 
 class BulkTransmitMessageTests: QuickSpec {
     override func spec() {
-        describe("init") {
+        describe("init from data") {
             it("data has appropriate structure") {
                 let sut = BulkTransmitMessage(
                     bulkID: [0x01, 0xBA, 0xBA, 0xBA, 0xBA, 0xBA, 0xBA, 0xBA, 0xBA, 0xBA, 0xBA, 0xBA, 0xBA, 0xBA, 0xBA, 0xFF],
@@ -26,13 +26,34 @@ class BulkTransmitMessageTests: QuickSpec {
                     0x02, // protocol version
                     0x01, 0xBA, 0xBA, 0xBA, 0xBA, 0xBA, 0xBA, 0xBA, 0xBA, 0xBA, 0xBA, 0xBA, 0xBA, 0xBA, 0xBA, 0xFF, // bulk id
                     0x01, 0x00, 0x00, 0x00, // type, uint32
-                    0x02, 0x00, 0x00, 0x00, // metadata length, uint32
+                    0x02, 0x00, 0x00, 0x00, // metadata length, little endian uint32
                     0xAA, 0xBB, // metadata
-                    0x04, 0x00, 0x00, 0x00, // content length, uint32
+                    0x04, 0x00, 0x00, 0x00, // content length, little endian uint32
                     0xCC, 0xDD, 0xEE, 0xFF // content
                 ]
 
                 expect(sut.data.bytes) == expectedBytesArray
+            }
+        }
+        describe("init from mobile bulk") {
+            it("does not throw") {
+                let bulkID = UUID(uuidString: "82f6ed49-b70d-4c9e-afa1-4b0377d0de5f")!
+                let mobileBulk = try! MobileBulk(bulkID: bulkID, type: .configBulk, metadata: "", content: "")
+                expect { try BulkTransmitMessage(mobileBulk: mobileBulk) }.toNot(throwError())
+            }
+
+            it("transforms data to properties") {
+                let uuidString = "82f6ed49-b70d-4c9e-afa1-4b0377d0de5f"
+                let metaData = "METADATA"
+                let content = "SFVG" // "HUF" base 64 encoded
+                let bulkID = UUID(uuidString: uuidString)!
+                let mobileBulk = try! MobileBulk(bulkID: bulkID, type: .configBulk, metadata: metaData, content: content)
+
+                let sut = try! BulkTransmitMessage(mobileBulk: mobileBulk)
+
+                expect(String(data: Data(sut.bulkID), encoding: .utf8)) == uuidString
+                expect(String(data: Data(sut.metadata), encoding: .ascii)) == metaData
+                expect(String(data: Data(sut.content), encoding: .utf8)) == "HUF"
             }
         }
     }
--- SecureAccessBLE/Classes/Models/Bulk/BulkTransmitMessage.swift
+++ SecureAccessBLE/Classes/Models/Bulk/BulkTransmitMessage.swift
@@ -15,7 +15,9 @@ struct BulkTransmitMessage: SorcMessagePayload {
     let content: [UInt8]
     let data: Data
 
-    private let messageID: UInt8 = 0x60
+    enum Error: Swift.Error {
+        case bulkIDFormat
+    }
 
     init(bulkID: [UInt8], type: UInt32, metadata: [UInt8], content: [UInt8]) {
         self.bulkID = bulkID
@@ -23,7 +25,7 @@ struct BulkTransmitMessage: SorcMessagePayload {
         self.metadata = metadata
         self.content = content
         var data = Data()
-        data.append(messageID)
+        data.append(SorcMessageID.bulkTranferPhoneResponse.rawValue)
         data.append(0x02) // protocol
         data.append(contentsOf: bulkID)
         data.append(type.data)
@@ -32,5 +34,15 @@ struct BulkTransmitMessage: SorcMessagePayload {
         data.append(UInt32(content.count).data)
         data.append(contentsOf: content)
         self.data = data
+    }
+
+    init(mobileBulk: MobileBulk) throws {
+        guard let bulkIDData = mobileBulk.bulkId.lowercasedUUIDString.data(using: .utf8) else {
+            throw Error.bulkIDFormat
+        }
+        self.init(bulkID: bulkIDData.bytes,
+                  type: mobileBulk.type.rawValue,
+                  metadata: mobileBulk.metadata.bytes,
+                  content: mobileBulk.content.bytes)
     }
 }
